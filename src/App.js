3abbdc86f6928c2640cada32b1c7190539cc0d83
Ali Mir <ali.mir@test.com>
2023-11-16 10:28:28 -0600

    change 02

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      var apiText = '';
+
+
+      apiText = await( await fetch(`/api/message?name=Ali`)).json();
+      console.log('apiText: ' + apiText);
+      setData(apiText);
+    })();
+  });
+
+  return <div>{data}</div>;
+}
+
+export default App;
+
+
+
+/*
+
 import React from 'react';
 
 function App() {
@@ -6,3 +31,5 @@ function App() {
 }
 
 export default App;
+
+*/
